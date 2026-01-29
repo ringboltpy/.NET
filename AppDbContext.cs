dd842fe3d9be8ee551a32e6cf6b10acf74631e68
ringboltpy <tikotavadze0@gmail.com>
2026-01-29 14:59:28 +0400

    Create AppDbContext.cs

---
+++ AppDbContext.cs
@@ -0,0 +1,24 @@
+using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Product> Products => Set<Product>();
+    public DbSet<Category> Categories => Set<Category>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        modelBuilder.Entity<Product>()
+            .Property(p => p.Price)
+            .HasColumnType("decimal(18,2)");
+    }
+}
